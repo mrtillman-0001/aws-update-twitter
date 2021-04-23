ab2afd7b548f2f0b9fdab795ac3274c928276d88
Stephen Tillman <askmrtillman@gmail.com>
2021-04-23 16:43:46 -0400

    isolate oauth timestamp + nonce

---
+++ nonce.js
@@ -0,0 +1,3 @@
+module.exports = {
+  nonce: "d72f7838-2e92-44b8-939b-5437e2fc929b"
+};
---
+++ now.js
@@ -0,0 +1,7 @@
+const _now = null;
+
+module.exports = () => {
+  if(_now) return _now;
+  _now = Math.floor(Date.now() / 1000)
+  return _now.toString();
+}
